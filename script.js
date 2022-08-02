e2ec4fc3158c351b031f147e61dc4da05ba68d89
rmbernardo27 <reannemaebernardo@gmail.com>
2022-08-02 17:45:30 +0800

    fix modal

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
-  modal.style.display = "block";
+  modal.style.display = "flex";
 }
 
 // When the user clicks on <span> (x), close the modal
